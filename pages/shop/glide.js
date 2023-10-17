44f23699e6cfb15266cbfafe552bef317ca84f81
Ailinludmila <146391289+Ailinludmila@users.noreply.github.com>
2023-10-17 00:30:10 -0300

    Update glide.js

--- pages/shop/glide.js
+++ pages/shop/glide.js
@@ -1,1 +1,17 @@
-
+    <script>
+        new Glide('.glide' {
+        type: 'carousel',
+        startAt: 0,
+        perView: 3,
+        gap: 30,
+        breakpoint: {
+            991: {
+                perView: 2
+            },
+            768: {
+                perView: 1
+            }
+        }
+}
+         ).mount();
+    </script>
